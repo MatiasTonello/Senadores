e692f24ac0837caed7f6607a28d33411ded9b8f3
Matias Tonello <matitonello@hotmail.com>
2020-02-04 05:18:03 -0300

    Senadores con Vue

---
+++ js/estadisticas.js
@@ -0,0 +1,116 @@
+var votosTotal = 0;
+var votosRep = 0;
+var votosDem = 0;
+var votosInd = 0;
+var members = data.results[0].members;
+
+
+for(var i = 0; i <members.length; i++){
+    var republicanos = members.filter(republic => republic.party == "R");
+    var democratas = members.filter(dem => dem.party == "D");
+    var independientes = members.filter(ind => ind.party == "I");
+    votosTotal += members[i].votes_with_party_pct
+
+
+    if(members[i].party == "R"){
+        votosRep += members[i].votes_with_party_pct;
+    }else if(members[i].party == "D"){
+        votosDem += members[i].votes_with_party_pct;
+    }else {
+        votosInd += members[i].votes_with_party_pct;
+    }
+    
+}
+
+votosDem = porcentaje(votosDem,votosTotal);
+votosInd = porcentaje(votosInd,votosTotal);
+votosRep = porcentaje(votosRep,votosTotal);
+
+
+function porcentaje(cantVotos, total){
+    cantVotos = (cantVotos * 100) / total;
+    cantVotos = Math.round(cantVotos * 100) / 100;
+    return cantVotos;
+}
+    votosTotal = votosDem + votosInd + votosRep;
+
+
+    var tabla ="<tr class=table-primary><th> Party </th> <th> Nro of Reps  </th><th> % Votes w/Party </th></tr>" + 
+        "<tr><td> Democrats </td> <td align ='center'>" + democratas.length + "</td> <td align ='center'>" + votosDem + "% </td></tr>" +
+        "<tr><td> Republicans </td> <td align ='center'>" + republicanos.length + "</td> <td align ='center'>" + votosRep + "% </td></tr>" +
+        "<tr><td> Independents </td> <td align ='center'>" + independientes.length + "</td> <td align ='center'>" + votosInd + "% </td></tr>" + 
+        "<tr><td> Total </td> <td align ='center'>" + data.results[0].members.length + "</td> <td align ='center'>" + votosTotal + "% </td></tr>";
+    
+    document.getElementById("table-data").innerHTML = tabla;
+
+
+var tablaBottom = "";
+var tablaTop = "";
+
+
+if(isLoyalty){
+    members.sort(function (a, b) {
+        if (a.votes_with_party_pct > b.votes_with_party_pct) {
+            return 1;
+        }
+        if (a.votes_with_party_pct < b.votes_with_party_pct) {
+            return -1;
+        } else {
+            return 0;
+        }
+    });
+    for(var i = 0; i < members.length; i++){
+        if(i < members.length * 0.1){
+            tablaBottom += "<tr><td> " + members[i].first_name + " "
+                + ((members[i].middle_name) != null ? members[i].middle_name : "") + " "
+                + ((members[i].last_name) != null ? members[i].last_name : "") + " </td>" +
+                "<td align ='center'>" + Math.round((members[i].total_votes * members[i].votes_with_party_pct) / 100) + "</td>" + 
+                "<td align ='center'>" + members[i].votes_with_party_pct + "%" + "</td></tr>";
+        }
+    }
+    members.reverse(); 
+        for(var i = 0; i < members.length; i++){
+            if(i < members.length * 0.1){
+            tablaTop += "<tr><td> " + members[i].first_name + " "
+                + ((members[i].middle_name) != null ? members[i].middle_name : "") + " "
+                + ((members[i].last_name) != null ? members[i].last_name : "") + " </td>" +
+                "<td align ='center'>" + Math.round((members[i].total_votes * members[i].votes_with_party_pct) / 100) + "</td>" + 
+                "<td align ='center'>" + members[i].votes_with_party_pct + "%" + "</td></tr>";
+        }
+    }
+}else {
+    members.sort(function (a, b) {
+        if (a.missed_votes_pct > b.missed_votes_pct) {
+            return 1;
+        }
+        if (a.missed_votes_pct < b.missed_votes_pct) {
+            return -1;
+        } else {
+            return 0;
+        }
+    });
+    for(var i = 0; i < members.length; i++){
+        if(i < members.length * 0.1){
+            tablaBottom += "<tr><td> " + members[i].first_name + " "
+                + ((members[i].middle_name) != null ? members[i].middle_name : "") + " "
+                + ((members[i].last_name) != null ? members[i].last_name : "") + " </td>" +
+                "<td align ='center'>" + members[i].missed_votes + "</td>" + 
+                "<td align ='center'>" + members[i].missed_votes_pct + "%" + "</td></tr>";
+        }
+    }
+    members.reverse();
+    for(var i = 0; i < members.length; i++){
+        if(i < members.length * 0.1){
+            tablaTop += "<tr><td> " + members[i].first_name + " "
+                + ((members[i].middle_name) != null ? members[i].middle_name : "") + " "
+                + ((members[i].last_name) != null ? members[i].last_name : "") + " </td>" +
+                "<td align ='center'>" + members[i].missed_votes + "</td>" + 
+                "<td align ='center'>" + members[i].missed_votes_pct + "%" + "</td></tr>";
+        }
+    }
+}
+
+
+
+document.getElementById("missed-votes").innerHTML = tablaBottom;
+document.getElementById("top-votes").innerHTML = tablaTop;
